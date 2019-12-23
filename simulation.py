e10ff77dad8402f0983981857b6fc41ab62ea905
Bogdan Vlad <vladbgd@gmail.com>
2019-12-23 17:59:52 +0200

    Minor changes.

--- simulation.py
+++ simulation.py
@@ -15,9 +15,27 @@
 
 # allocate initial amount of money
 
+
+class Individuals:
+    def __init__(self, strategy, endowment, individualType):
+        self.strategy = strategy
+        self.endowment = endowment
+        self.type = individualType
+
+class Game: 
+    def select():
+        # returns two elements
+
+    def play():
+        select()
+
+
 for generation in range(0, number_of_generations):
     for game in range(o, n):
         # select group of K random players
+        game = Game()
+
+        game.play()
 
         # get money
         for round in range(0, n):
