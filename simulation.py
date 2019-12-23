37449e2c1fcf69ebf5d8f2e56d6e0c52c1886742
Bogdan Vlad <vladbgd@gmail.com>
2019-12-23 17:42:57 +0200

    Initial commit.

---
+++ simulation.py
@@ -0,0 +1,33 @@
+#parameters: 
+    # - group size
+    # - number of generations
+    # - population size
+    # - number of games 
+    # - number of rounds 
+    # - alpha value 
+    # - lambda value
+    # - rich / poor scenario
+    # - fitness function
+    # - risk function
+    # - 
+
+# allocate random strategies to all players
+
+# allocate initial amount of money
+
+for generation in range(0, number_of_generations):
+    for game in range(o, n):
+        # select group of K random players
+
+        # get money
+        for round in range(0, n):
+            # invest
+
+            # check for possibility of loss
+
+    # mutate the generation
+
+    # keep track of contribution
+
+
+        
