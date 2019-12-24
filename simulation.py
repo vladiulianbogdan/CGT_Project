179746e4c88a90cdc0b6f02a29dd7ae9035f07b9
madave94 <david.tschirschwitz@uni-weimar.de>
2019-12-24 12:25:12 +0100

    added class Individual with docstring

--- simulation.py
+++ simulation.py
@@ -1,35 +1,60 @@
-#parameters: 
+#parameters:
     # - group size
     # - number of generations
     # - population size
-    # - number of games 
-    # - number of rounds 
-    # - alpha value 
+    # - number of games
+    # - number of rounds
+    # - alpha value
     # - lambda value
     # - rich / poor scenario
     # - fitness function
     # - risk function
-    # - 
+    # -
 
 # allocate random strategies to all players
 
 # allocate initial amount of money
 
+import numpy as np
 
-class Individuals:
-    def __init__(self, strategy, endowment, individualType):
-        self.strategy = strategy
+class Individual:
+    """A single individual with the information it has in the system
+
+    Attributes:
+        endowment(float): the current wealth of the individual
+        strategy(np.array(tau, a, b)): the strategy set of the individual
+            tau: threshold
+            a: result if the threshold is smaller or equal then the given value
+            b: result if the threshold is bigger
+        type(boolean): either rich or poor individual
+    """
+    def __init__(self, endowment, strategy, individualType):
+
         self.endowment = endowment
+        self.strategy = strategy
         self.type = individualType
 
-class Game: 
+    def __repr__(self):
+        return "[Wealth = " + str(self.endowment) + "]\nStrategy:\n"+ str(self.strategy)
+
+class Population:
+    def __init__(self, populationSize, initializationFunction):
+        self.populationSize = populationSize
+        self.population = []
+
+    def createPopulation(self):
+        for newIndividual in range(0, self.populationSize):
+            population.append(initializationFunction())
+
+class Game:
     def select():
         # returns two elements
+        pass
 
     def play():
         select()
 
-
+"""
 for generation in range(0, number_of_generations):
     for game in range(o, n):
         # select group of K random players
@@ -46,6 +71,8 @@ for generation in range(0, number_of_generations):
     # mutate the generation
 
     # keep track of contribution
+"""
 
-
-        
+strategyOne = np.array([[0.0,0.1,0.0], [0.2,0.1,0.5]])
+individual = Individual(1,strategyOne,False)
+print(individual)
