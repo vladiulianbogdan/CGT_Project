1401052971c621141fbda54fbec3f5c518b26302
Lennart_WSL <lennart.tuijnder@icloud.com>
2020-01-02 12:55:06 +0100

    Removed alpha=1
    Removed case alpha=1 since this is run in figure2 and so redundant

--- figure1_V1.sh
+++ figure1_V1.sh
@@ -8,7 +8,7 @@ for riskFunctionParameters in "1 1" "2 10" "3 7" "1 10"
     	for rounds in 1 2 4
     		do 
                 set -- $riskFunctionParameters
-        		for alpha in 0.0 0.2 0.4 0.6 0.8 1.0
+        		for alpha in 0.0 0.2 0.4 0.6 0.8
         			do
             			python3 simulation.py $GEN $rounds 2 100 1 $alpha $alpha 1000 1 1 $1 0 $2 figure1_V1_ &
         			done
